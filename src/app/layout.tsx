d35f215d56173d37f7cafcd155cb9fa213f4d178
Ryoichi Hayashi <dtorannpu@gmail.com>
2023-07-09 16:03:27 +0900

    不要なものを削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
-'use client';
-
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { UserProvider, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
-import Header from '@/components/Header';
-import { Suspense } from 'react';
-import Loading from './loading';
 
 
 const inter = Inter({ subsets: ['latin'] })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-'use client';
-
-import Image from 'next/image'
-import { useUser } from '@auth0/nextjs-auth0/client';
 import Link from 'next/link';
 
 export default function Home() {
