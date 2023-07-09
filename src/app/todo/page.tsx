45a7d4a53b6d556a2a913d6f77f8572d153bf16d
Ryoichi Hayashi <dtorannpu@gmail.com>
2023-07-09 12:35:37 +0900

    テーブル作成

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -5,6 +5,21 @@ export default withPageAuthRequired(async function Page() {
     return (
         <div>
             <h1 className="text-3xl">Todo</h1>
+
+            <table className="table-auto">
+                <thead>
+                    <tr>
+                        <th>タイトル</th>
+                        <th>内容</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>タイトル</td>
+                        <td>内容</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 });
