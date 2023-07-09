673ef20b0b12fa77da90cc4a1fcebf0149ee9c0b
Ryoichi Hayashi <dtorannpu@gmail.com>
2023-07-09 11:52:13 +0900

    ヘッダ調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,22 +7,20 @@ const Header = () => {
     const { user, isLoading } = useUser();
     return (
         <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/todo">TODO</Link>
-                    </li>
-                    {!isLoading && !user && (
-                        <li>
-                            <a href="/api/auth/login">ログイン</a>
-                        </li>
-                    )}
-                    {!isLoading && user && (
-                        <li>
-                            <a href="/api/auth/logout">ログアウト</a>
-                        </li>
-                    )}
-                </ul>
+            <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+                <div className="w-full block flex-grow lg:items-center lg:w-auto">
+                    <div className="text-sm lg:flex-grow">
+                        <Link href="/todo" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">TODO</Link>
+
+                        {!isLoading && !user && (
+
+                            <a href="/api/auth/login" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">ログイン</a>
+                        )}
+                        {!isLoading && user && (
+                            <a href="/api/auth/logout" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">ログアウト</a>
+                        )}
+                    </div>
+                </div>
             </nav>
         </header>
     );
