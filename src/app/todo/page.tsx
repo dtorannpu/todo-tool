45b225282cb4495c0250e068a115153fdeffb161
Ryoichi Hayashi <dtorannpu@gmail.com>
2023-07-09 11:29:00 +0900

    TODOページ作成

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -4,7 +4,7 @@ import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 export default withPageAuthRequired(async function Page() {
     return (
         <div>
-            <h1>Todo</h1>
+            <h1 className="text-3xl">Todo</h1>
         </div>
     );
 });
