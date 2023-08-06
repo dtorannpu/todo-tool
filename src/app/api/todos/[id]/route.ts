74b2c2f8ddcb05f7e2c93e7d9b059b7df475a5f6
Ryoichi Hayashi <dtorannpu@gmail.com>
2023-08-06 09:23:30 +0900

    照会画面追加

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,22 @@
+import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { NextResponse } from "next/server";
+
+const GET = withApiAuthRequired(async (req, ctx) => {
+    const { accessToken } = await getAccessToken();
+    const id = ctx.params?.id;
+    const res = await fetch(`http://localhost:8080/todos/${id}`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json'
+        },
+    });
+
+    if (!res.ok) {
+        throw new Error('データが取得できませんでした。');
+    }
+    const data = await res.json();
+    return NextResponse.json(data);
+});
+
+export { GET };
--- src/app/todo/[id]/page.tsx
+++ src/app/todo/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { getAccessToken } from "@auth0/nextjs-auth0";
+'use client'
+
 import Link from "next/link";
 
 interface TodoItem {
@@ -8,11 +9,8 @@ interface TodoItem {
 }
 
 async function getTodo(id: number) {
-    const { accessToken } = await getAccessToken();
-    const res = await fetch(`http://localhost:8080/todos/${id}`, {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-        },
+    const res = await fetch(`http://localhost:3000/api/todos/${id}`, {
+        
         cache: 'no-store'
     });
 
@@ -21,6 +19,10 @@ async function getTodo(id: number) {
     }
 
     return res.json();
+}
+
+async function deleteTodo(id: number) {
+
 }
 
 export default async function Page({ params }: { params: { id: number } }) {
@@ -36,7 +38,7 @@ export default async function Page({ params }: { params: { id: number } }) {
                 {todo.description}
             </div>
             <Link href="/todo" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">戻る</Link>
-            {/* <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => deleteTodo(todo.id)}>削除</button> */}
+            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => deleteTodo(todo.id)}>削除</button>
         </div>
     );
 }
