bddc3b2350e2716bff2b6ddcff94f7af43ebb835
Ryoichi Hayashi <dtorannpu@gmail.com>
2023-08-11 12:58:48 +0900

    テスト修正

--- jest.config.ts
+++ jest.config.ts
@@ -1,25 +1,12 @@
-import type { Config } from 'jest';
+import type { Config } from "jest";
 import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
-    dir: './'
+    dir: './',
 });
 
 const config: Config = {
-    setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-    moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
-    collectCoverageFrom: [
-        '<rootDir>/**/*.{js,jsx,ts,tsx}',
-        '<rootDir>/src/app/page.tsx',
-        '!**/*.d.ts',
-        '!**/node_modules/**',
-        '!<rootDir>/out/**',
-        '!<rootDir>/.next/**',
-        '!<rootDir>/*.config.{js,ts}',
-        '!<rootDir>/coverage/**',
-        '!<rootDir>/jest-setup.ts',
-    ]
 };
 
 export default createJestConfig(config);
