71dd046cf2733cb0978d43aeaceec0cab4878293
Ryoichi Hayashi <dtorannpu@gmail.com>
2023-08-20 14:06:13 +0900

    TODO追加 #2

--- setup.ts
+++ setup.ts
@@ -1,3 +1,5 @@
+// TODO https://github.com/jsdom/jsdom/issues/2524
+// https://github.com/dtorannpu/todo-app/issues/2
 import { TextDecoder, TextEncoder } from 'util';
 
 (global as any).TextEncoder = TextEncoder;
