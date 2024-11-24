5c15a050837e18f6496084ca8ea9ec688c329bf6
dtorannpu <dtorannpu@gmail.com>
2024-11-24 13:17:49 +0900

    apiurl修正

--- backend/src/main/kotlin/com/example/Application.kt
+++ backend/src/main/kotlin/com/example/Application.kt
@@ -16,7 +16,9 @@ import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain
+        .main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module(koinModules: List<Module> = listOf(appModule)) {
